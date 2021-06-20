780fef660023fd915e47bdc747bc7ed36f41c708
raf-wtag <fahim.rahman@welldev.io>
2021-06-20 12:01:38 +0600

    UIWindow added in AppDelegate
    UIWindow type variable is needed n appdelegate for iOS 12 abd below versions since the SceneDelegate is for scene controll in splitview for iOS 13 or above

--- Messenger/Delegates/AppDelegate.swift
+++ Messenger/Delegates/AppDelegate.swift
@@ -13,7 +13,8 @@ import GoogleSignIn
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
--- Messenger/Delegates/SceneDelegate.swift
+++ Messenger/Delegates/SceneDelegate.swift
@@ -10,9 +10,8 @@ import FBSDKCoreKit
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
